52f4fa6817fa185bd7d12eb5c6bffd5a6faf4b0a
Ryan J. O'Neil <ryanjoneil@gmail.com>
2015-04-03 14:08:28 -0400

    using -u for edit.sh

--- edit.sh
+++ edit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+U="dmfds"
 W="/home/dmfds/ipynb"
 V="$PWD/ipynb:$W:rw"
 I="dmfds"
-C="sudo su - dmfds"
 
-docker run -i -t --rm=true -v=$V $I /bin/bash -c "$C"
+docker run -i -t --rm=true -u=$U -w=$W -v=$V $I
