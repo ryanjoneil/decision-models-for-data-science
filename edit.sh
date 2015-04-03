4f3bc0f490b0d06d0d9d73f7eb3c19e1f0911a71
Ryan J. O'Neil <ryanjoneil@gmail.com>
2015-04-03 13:09:49 -0400

    adding user account for docker processes to run as

--- edit.sh
+++ edit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-VOL="$PWD/ipynb:/ipynb:rw"
-docker run -i -t --rm=true -v=$VOL -w=/ipynb dmfds /bin/bash 
+W="/home/dmfds/ipynb"
+V="$PWD/ipynb:$W:rw"
+I="dmfds"
+C="sudo su - dmfds"
 
+docker run -i -t --rm=true -v=$V $I /bin/bash -c "$C"
--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-VOL="$PWD/ipynb:/ipynb:rw"
-docker run --rm=true --name dmfds-ipynb -p 8888:8888 -v=$VOL -w=/ipynb dmfds \
-	/bin/bash -c "ipython3 notebook --ip=0.0.0.0 --port=8888 --no-browser"
+N="dmfds-ipynb"
+P="8888:8888"
+W="/home/dmfds/ipynb"
+V="$PWD/ipynb:$W:rw"
+I="dmfds"
+C="sudo -H -u dmfds ipython3 notebook --ip=0.0.0.0 --port=8888 --no-browser"
+
+docker rm -f $(docker ps -a | grep $N | awk '{print $1}')
+docker run -d --name $N -p $P -w=$W -v=$V $I /bin/bash -c "$C"
