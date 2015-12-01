ebf8aa2526bec80c0bb223e7afe0f9124fb74f46
Ryan J. O'Neil <ryanjoneil@gmail.com>
2015-12-01 10:58:15 -0500

    updating docker image to use anaconda

--- build.sh
+++ build.sh
@@ -4,27 +4,21 @@ UID=$(id -u $USER)
 GID=$(id -g $USER)
 
 cat <<EOF > Dockerfile
-FROM ubuntu:utopic
+FROM ubuntu:trusty
 
-RUN apt-get update
-RUN apt-get install -y glpk-utils
-RUN apt-get install -y libblas-dev
-RUN apt-get install -y liblapack-dev
-RUN apt-get install -y python3-matplotlib
-RUN apt-get install -y python3-numpy
-RUN apt-get install -y python3-pip
-RUN apt-get install -y python3-scipy
+RUN apt-get install -y wget
 
-RUN pip3 install cvxopt==1.1.7
-RUN pip3 install ipython[all]==3.0.0
-RUN pip3 install pulp==1.5.8
-RUN pip3 install scikit-learn==0.16.0
+RUN wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.0-Linux-x86_64.sh
+RUN bash /Anaconda3-2.4.0-Linux-x86_64.sh -b -p /opt/conda3
+RUN rm /Anaconda3-2.4.0-Linux-x86_64.sh
+RUN /opt/conda3/bin/pip install pulp==1.6.0
 
 RUN groupadd -r dmfds -g $GID
 RUN useradd -r -g dmfds -u $UID -m -d /home/dmfds -s /bin/bash \
 	-c "Decision Models for Data Science" dmfds
 RUN mkdir /home/dmfds/ipynb
 RUN chown dmfds:dmfds /home/dmfds/ipynb
+
 EOF
 
 docker build --force-rm=true --tag="dmfds" .
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ P="8888:8888"
 W="/home/dmfds/ipynb"
 V="$PWD/ipynb:$W:rw"
 I="dmfds"
-C="sudo -H -u dmfds ipython3 notebook --ip=0.0.0.0 --port=8888 --no-browser"
+C="sudo -H -u dmfds /opt/conda3/bin/ipython notebook --ip=0.0.0.0 --port=8888 --no-browser"
 
 docker rm -f $(docker ps -a | grep $N | awk '{print $1}')
 docker run -d --name $N -p $P -w=$W -v=$V $I /bin/bash -c "$C"
